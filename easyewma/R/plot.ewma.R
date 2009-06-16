b141289a03f2bba9c16f4a6eadb992b6a82f936b
Paolo Bosetti <paolo.bosetti@me.com>
2009-06-16 16:24:57 +0200

    updated plot.ewma

--- easyewma/R/plot.ewma.R
+++ easyewma/R/plot.ewma.R
@@ -1,8 +1,6 @@
 plot.ewma <-
-function(x, typ="b", xlab="Index", ylab="EWMA",...) {
+function(x, typ="b", xlab="Index", ylab="EWMA", ylim=c(min(x$data$lcl), max(x$data$z)), xlim=c(0,max(x$data$i)), ...) {
   plot(x$data$z~x$data$i, 
-    ylim=c(min(x$data$lcl),max(x$data$z)),
-    xlim=c(0,max(x$data$i)),
     typ = typ, 
     xlab=xlab,
     ylab=ylab,
